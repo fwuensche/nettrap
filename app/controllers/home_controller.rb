e4f97581d1c65f02451803a6b71b3fb80e75fa22
Flavio Wuensche <flavio@touts.com.br>
2018-05-27 12:43:03 -0400

    Allow for direct image downloading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
+require 'open-uri'
+
 class HomeController < ApplicationController
   def index
     @images = Unsplash::Photo.search("pattern")
+  end
+
+  def download_image
+    data = open(resized_url(url: params[:url], w: params[:w], h: params[:h]))
+    send_data data.read, type: "image/jpeg", disposition: "attachment",
+      filename: "#{params[:image_id]}_#{params[:w]}x#{params[:h]}.jpg"
+  end
+
+  private
+
+  def resized_url(url:, w:, h:)
+    url.split("?").first + "?w=#{w}&h=#{h}&fit=crop"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get "download_image", to: "home#download_image", as: :download_image
 end
